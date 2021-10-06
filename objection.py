32244704707c3e6755aaac1919d559c10f2dc9ae
rebecca.bruehlman <rebecca.bruehlman@nielsen.com>
2021-10-05 22:25:04 -0400

    guess we'll add gunicorn

--- objection.py
+++ objection.py
@@ -1,11 +1,27 @@
+import logging
 import os
 import random
+
+from flask import Flask, request
+from slack_bolt.adapter.flask import SlackRequestHandler
 from slack_bolt import App
+
+
+logging.basicConfig(level='INFO')
+logging.info('Logging now setup.')
 
 app = App(
     token=os.environ.get("USER_OATH_TOKEN"),
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 )
+
+flask_app = Flask(__name__)
+handler = SlackRequestHandler(app)
+
+
+@flask_app.route("/slack/events", methods=["POST"])
+def slack_events():
+    return handler.handle(request)
 
 
 def _is_text(event):
@@ -27,6 +43,7 @@ def respond(client, event, logger):
     if _is_text(event) and _is_channel_or_dm(event) and _is_not_self(event):
 
         seed = random.choice(range(200))
+        logger.info(f"Seed: {seed}")
 
         if seed == 1:
             try:
@@ -46,7 +63,3 @@ def respond(client, event, logger):
 
             except Exception as e:
                 logger.error(f"Error objecting: {e}")
-
-
-if __name__ == "__main__":
-    app.start(port=int(os.environ.get("PORT", 3000)))
